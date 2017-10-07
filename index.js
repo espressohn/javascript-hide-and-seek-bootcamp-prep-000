e54426f9bb024665716cd95196f95db59194ce36
espressohn <espressohn@gmail.com>
2017-10-07 19:11:33 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+function getFirstSelector(selector){
+    return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.getElementById('nested')
+}
+
+function increaseRankBy(n){
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = (parseInt(lis[i].innerHTML) + n).toString()
+  }
+}
+
+function deepestChild(){
+
+}
