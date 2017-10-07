e5c06b1b6d508db860a4e47eaee37bd6decc46b7
espressohn <espressohn@gmail.com>
2017-10-07 20:01:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function increaseRankBy(n){
 function deepestChild(){
   const lis = document.querySelectorAll('div#grand-node')
   console.log(lis)
-   return lis[lis.length]
+   return lis[lis.length-1]
   //for (let i = 0; i < lis.length; i++) {
     //lis[i].innerHTML = (parseInt(lis[i].innerHTML) + n).toString()
 //  }
